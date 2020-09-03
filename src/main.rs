9131041f7b4cd9a8d82ffd2a4ee9ee5fefaaf850
layonthehorn <thomaspdressel@gmail.com>
2020-09-02 21:22:05 -0700

    Bad error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 use glob::glob;
 use std::path::PathBuf;
 
-fn main() {
+
+fn main(){
     // gets current directory or ends if unreadable
-    let pwd = match std::env::current_dir() {
-        Ok(path) => path,
-        Err(e) => panic!("Could not read directory, {}",e)
+    let result = std::env::current_dir();
 
-    };
+    // checks if an error was returned
+    if result.is_err(){
+        println!("Can not read directory.");
 
-    // getting the pattern to match
-    let pattern: String = create_pattern(&pwd);
-    println!("{}", pattern)
+    } else {
+        let cwd = result.unwrap();
+        let match_string = create_pattern(&cwd);
+        list_files(match_string);
+
+        };
+
 
 
 }
 
-fn create_pattern(path: &PathBuf) -> String{
+
+fn create_pattern(path: &PathBuf) -> String {
     let match_string = path.join("*");
-    let mut return_string = match_string.to_str().unwrap_or_else(||{
-        panic!("Could not create pattern string.")
-    });
+    let return_string = match_string.to_str().expect("Could not create pattern string.");
     return_string.to_string()
+}
+
+fn list_files(matcher: String){
+    for entry in glob(&matcher).unwrap(){
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
+
+    }
 }
