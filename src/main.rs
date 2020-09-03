a7f904467fe9f9bb474062f6d133326522925b14
layonthehorn <thomaspdressel@gmail.com>
2020-09-02 19:32:26 -0700

    Getting current working directory, and creating a string to match with.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use glob::glob;
+use std::path::PathBuf;
 
 fn main() {
-    // gets current working directory
-    // no error handling for now
-    let pwd = std::env::current_dir().unwrap();
-    println!("The currect dir is {}", pwd.display());
+    // gets current directory or ends if unreadable
+    let pwd = match std::env::current_dir() {
+        Ok(path) => path,
+        Err(e) => panic!("Could not read directory, {}",e)
 
+    };
+
+    // getting the pattern to match
+    let pattern: String = create_pattern(&pwd);
+    println!("{}", pattern)
+
+
+}
+
+fn create_pattern(path: &PathBuf) -> String{
+    let match_string = path.join("*");
+    let mut return_string = match_string.to_str().unwrap_or_else(||{
+        panic!("Could not create pattern string.")
+    });
+    return_string.to_string()
 }
