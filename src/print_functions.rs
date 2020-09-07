13b62c13e4cd0f2eb3a050948b138cdbde0fab5f
layonthehorn <thomaspdressel@gmail.com>
2020-09-07 10:07:55 -0700

    Added start of file permissions and sub directory count.

--- src/print_functions.rs
+++ src/print_functions.rs
@@ -132,12 +132,6 @@ fn get_file_base_name(path: &PathBuf) -> String {
 // gets the attributes of a file
 fn get_attributes(file: &PathBuf) -> String {
     let convert_kb = 1000;
-
-    // gets number of files inside directory
-    let number_files = match read_dir(file) {
-        Ok(files) => files.count().to_string(),
-        Err(_e) => "1".to_string(),
-    };
     let file_or_dir = if file.is_dir() { 'D' } else { 'F' };
     // attempts to get metadata if possible
     match file.metadata() {
@@ -146,9 +140,10 @@ fn get_attributes(file: &PathBuf) -> String {
             let user_data = get_user_metadata(&meta);
 
             format!(
-            "{}{:>3} {:>5} {:>5}{:>7},{}",
+            "{}{}{:>3} {:>5} {:>5}{:>7},{}",
             file_or_dir,
-            number_files,
+            user_data.file_mode,
+            get_subdirectory_count(&file),
             user_data.user_name, // to be user id
             user_data.group_name, // to be group id
             meta.len() / convert_kb,
@@ -164,16 +159,48 @@ fn get_attributes(file: &PathBuf) -> String {
 
 struct UserData{
     user_name: String,
-    group_name: String
+    group_name: String,
+    file_mode: String,
+}
+
+// gets the number of sub directories in a folder
+fn get_subdirectory_count(path: &PathBuf) -> String {
+    // if its a directory return the count of directories inside it
+    let count = if path.is_dir(){
+        let mut inner_count = 2;
+        for entry in read_dir(path).unwrap() {
+            match entry {
+                Ok(t) => {
+                    if t.path().is_dir(){
+                        inner_count += 1;
+                    }
+                }
+                Err(_e) => {}
+            }
+        }
+        inner_count
+
+    } else {
+        // if its a file return one
+        1
+
+    };
+    count.to_string()
+
 }
 
 fn get_user_metadata(meta: &Metadata) -> UserData {
     let user_id = get_user_by_uid(meta.uid());
     let group_id = get_group_by_gid(meta.gid());
+    let user_mode = meta.mode();
+
+    // getting username of file owner
     let user = match &user_id {
         Some(u) =>{u.name().to_str().unwrap_or_else(|| {"DNE"})}
         None => {"DNE"}
     };
+
+    // getting group of file owner
     let group = match &group_id {
         Some(g)=> { g.name().to_str().unwrap_or_else(||{"DNE"})
 
@@ -183,5 +210,10 @@ fn get_user_metadata(meta: &Metadata) -> UserData {
     UserData{
         user_name: user.to_string(),
         group_name: group.to_string(),
+        file_mode: create_unix_file_mode(user_mode),
     }
+}
+
+fn create_unix_file_mode(number: u32) -> String {
+   number.to_string()
 }
